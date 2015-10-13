50ab91020ef26e2cc943a82c0253151d912cf6ac
Andrew Joia <drewdrewthis@gmail.com>
2015-10-13 10:23:43 -0400

    Lists out functions of app

--- js/app.js
+++ js/app.js
@@ -12,6 +12,22 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	// New Game Function 
+
+  	// Create Secret Number
+
+  	// Accept/Evaluate User Input
+
+  	// Compare User Input to Secret Number
+
+  	// Tell the User Hot or Cold
+
+  	// Add Guess to Guess List
+
+  	// Count Guesses
+
+
+
 });
 
 
